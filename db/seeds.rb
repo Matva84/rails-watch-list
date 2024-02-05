ec7f103362a60bc8a9a6a64efa3054f1f8556509
Mathieu VALLEAU <mathieuvalleau@hotmail.fr>
2024-02-05 21:18:37 +0100

    Seed générée

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+URL = ["https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg"]
+
+puts "Cleaning database..."
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+
+puts "Creating movies..."
+
+15.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.sentences,
+    poster_url: URL[rand(0..3)],
+    rating: rand(0..100)/10
+  )
+  movie.save
+end
+
+puts "Finished"
